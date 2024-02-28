71cc8d2c02de74b7f1a0e928c85341548c2a4de8
Globe <92859545+GlobeReverse@users.noreply.github.com>
2024-02-28 23:42:44 +1100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,23 +2,9 @@ document.addEventListener("DOMContentLoaded", function() {
     const overlay = document.getElementById("overlay")
     const enterButton = document.getElementById("enterButton")
     const backgroundVideo = document.getElementById("backgroundVideo")
-    const RandomChooser = Math.round((Math.random() * 4) + 1)
 
     backgroundVideo.controls = false
-    
-    switch (RandomChooser) {
-        case 4:
-            backgroundVideo.src = "https://cdn.discordapp.com/attachments/718589480285765662/1195608109398376448/Bis_X_Blanco_X_Active_X_MizOrMac_-_Kennington_Where_It_Started_HarlemSpartans_-_Trim.mp4?ex=65b49bd2&is=65a226d2&hm=e2f927d59c8abb681e9157d03831d9a8838a97d548a33d873e495315e80614bb&"
-            break 
-        case 3:
-            backgroundVideo.src = "https://cdn.discordapp.com/attachments/718589480285765662/1195608109398376448/Bis_X_Blanco_X_Active_X_MizOrMac_-_Kennington_Where_It_Started_HarlemSpartans_-_Trim.mp4?ex=65b49bd2&is=65a226d2&hm=e2f927d59c8abb681e9157d03831d9a8838a97d548a33d873e495315e80614bb&" 
-            break
-        case 2:
-            backgroundVideo.src = "https://cdn.discordapp.com/attachments/718589480285765662/1195608912284631050/TPL_Sava_x_Bm_x_Mini_OTP_-_Haringey_Borough_Music_Video_-_Trim.mp4?ex=65b49c92&is=65a22792&hm=28599f3356f21e136ea3596e46778bea73ae5f02ce7e15827204c85a21253d2d&"
-            break
-        default:
-            backgroundVideo.src = "https://cdn.discordapp.com/attachments/718589480285765662/1195608912284631050/TPL_Sava_x_Bm_x_Mini_OTP_-_Haringey_Borough_Music_Video_-_Trim.mp4?ex=65b49c92&is=65a22792&hm=28599f3356f21e136ea3596e46778bea73ae5f02ce7e15827204c85a21253d2d&"
-    }
+    backgroundVideo.src = "https://cdn.discordapp.com/attachments/1206489703457628182/1212379211663151144/Song_2_1.mp4?ex=65f19f20&is=65df2a20&hm=63bcc8f799f9ba1dd405f2263f68ec17e89e2f9c1862188990ec94dff47e20b9&"
 
     enterButton.addEventListener("click", function() {
         backgroundVideo.play()
